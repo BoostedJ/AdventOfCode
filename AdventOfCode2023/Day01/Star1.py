55553b83ef62e52a78056ec34837fc7f6810328e
BoostedJ <justin.pelak7@gmail.com>
2023-12-01 17:24:43 -0800

    Star 1 complete

--- AdventOfCode2023/Day01/Star1.py
+++ AdventOfCode2023/Day01/Star1.py
@@ -1,3 +1,20 @@
 # Advent of Code 2023
 # Day 1, Star 1
 
+# star_1_input.txt is my specific puzzle inputs
+input = open("star_1_input.txt", "r")
+lines = input.readlines()
+
+total = 0
+for line in lines:
+    first = None
+    last = None
+    for char in line:
+        if char.isdigit():
+            if first:
+                last = char
+            else:
+                first = char
+                last = char
+    total += int(str(first + last))
+print(total)
