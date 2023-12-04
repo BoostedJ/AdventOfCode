68daa910100013696fbb40a02679f3857420a84b
BoostedJ <justin.pelak7@gmail.com>
2023-12-03 19:05:49 -0800

    Completed Star1

--- AdventOfCode2023/Day01/Star2.py
+++ AdventOfCode2023/Day01/Star2.py
@@ -1,3 +1,11 @@
 # Advent of Code 2023
 # Day 1, Star 2
 
+"""
+problem in my words:
+
+with that same file,
+    1. the total must now include numbers that are spelled out, (e.g. one2three will now result in 13)
+    2. combine the resulting number into a total
+    3. print the total, that is the answer
+"""
